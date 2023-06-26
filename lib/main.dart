1175e89a04f764578aa3680ae283e55590bbe300
Kingtous <kingtous@qq.com>
2023-06-26 08:42:30 +0800

    FIXME: second issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,293 +1,67 @@
-import 'dart:math';
-import 'dart:typed_data';
-
+import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'dart:ui' as ui;
 
 void main() {
-  runApp(App());
+  runApp(const MyApp());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Stack(
-        children: [
-          Home(),
-        ],
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Demo()),
     );
   }
 }
 
-class Home extends StatefulWidget {
-  const Home({super.key});
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  var rotateX = 0.0;
-  var value = 0.5;
-  final key = GlobalKey<State>();
-  var flipRateScale = 2.0;
-  Uint8List? img;
-
+class Demo extends StatelessWidget {
+  const Demo({super.key});
   @override
   Widget build(BuildContext context) {
-    var transform = Matrix4.identity()..setEntry(3, 2, 0.001);
-    // print(transform);
-    return Scaffold(
-      body: Row(
-        children: [
-          Expanded(
-              child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(32.0),
-                child: Column(
-                  children: [
-                    const Text(
-                      "With RepaintBoundary",
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                    Transform(
-                      transform: transform.clone()
-                        ..rotateX(-value * flipRateScale),
-                      child: Container(
-                        color: Colors.red,
-                        width: 150,
-                        height: 150,
-                        alignment: Alignment.center,
-                        child: RepaintBoundary(
-                          key: key,
-                            child: Stack(
-                              // mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Container(
-                                  child: Transform(
-                                    // key: key,
-                                    transform: transform.clone()
-                                      ..rotateY(-value * flipRateScale),
-                                    child: Container(
-                                      color: Colors.black,
-                                      width: 60,
-                                      height: 60,
-                                    ),
-                                  ),
-                                ),
-                                Text("tes")
-                              ],
-                            )),
-                      ),
-                    ),
-                    // Material(
-                    //   child: Container(
-                    //     child: Text("${transform}"),
-                    //   ),
-                    // ),
-
-                    // Material(
-                    //   child: Transform(
-                    //     transform: Matrix4.skewX(0.1)..rotateX(rotateX),
-                    //     child: ColoredBox(
-                    //       color: Colors.black,
-                    //       child: Transform(
-                    //         alignment: Alignment.topRight,
-                    //         transform: Matrix4.skewX(0.1)..rotateX(rotateX),
-                    //         child: Container(
-                    //           padding: const EdgeInsets.all(8.0),
-                    //           color: const Color(0xFFE8581C),
-                    //           child: const Text('测试！'),
-                    //         ),
-                    //       ),
-                    //     ),
-                    //   ),
-                    // ),
-                    const SizedBox(
-                      height: 8.0,
-                    ),
-                    Material(
-                      child: SizedBox(
-                        width: 100,
-                        height: 10,
-                        child: Slider(
-                            value: value,
-                            onChanged: (v) {
-                              setState(() {
-                                rotateX = 2 * pi * v;
-                                value = v;
-                              });
-                              Future.delayed(Duration.zero, () {
-                                final bound =
-                                    key.currentContext?.findRenderObject()
-                                        as RenderRepaintBoundary;
-                                // bound.toImage().then((value) async {
-                                //   final img = await value.toByteData(
-                                //       format: ui.ImageByteFormat.png);
-                                //   setState(() {
-                                //     this.img = img?.buffer.asUint8List();
-                                //   });
-                                // });
-                              });
-                            }),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 8.0,
-                    ),
-
-                    Column(
-                      children: [
-                        TextButton(
-                            onPressed: () {
-                              final bound = key.currentContext
-                                  ?.findRenderObject() as RenderRepaintBoundary;
-                              // bound.toImage().then((value) async {
-                              //   final img = await value.toByteData(
-                              //       format: ui.ImageByteFormat.png);
-                              //   setState(() {
-                              //     this.img = img?.buffer.asUint8List();
-                              //   });
-                              // });
-                            },
-                            child: const Text("Catch Images")),
-                        img == null
-                            ? Text("img")
-                            : Image.memory(
-                                img!,
-                                width: 100,
-                                height: 100,
-                                fit: BoxFit.contain,
-                              )
-                      ],
-                    )
-                  ],
-                ),
-              )
-            ],
-          )),
-          Expanded(
-              child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(32.0),
-                child: Column(
-                  children: [
-                    const Text("Without RepaintBoundary"),
-                    Transform(
-                      transform: transform.clone()
-                        ..rotateX(-value * flipRateScale),
-                      child: Container(
-                        color: Colors.red,
-                        width: 150,
-                        height: 150,
-                        alignment: Alignment.center,
-                        child: Transform(
-                          // key: key,
-                          transform: transform.clone()
-                            ..rotateY(-value * flipRateScale),
-                          child: Container(
-                            color: Colors.black,
-                            width: 60,
-                            height: 60,
-                          ),
-                        ),
-                      ),
-                    ),
-                    // Material(
-                    //   child: Container(
-                    //     child: Text("${transform}"),
-                    //   ),
-                    // ),
-
-                    // Material(
-                    //   child: Transform(
-                    //     transform: Matrix4.skewX(0.1)..rotateX(rotateX),
-                    //     child: ColoredBox(
-                    //       color: Colors.black,
-                    //       child: Transform(
-                    //         alignment: Alignment.topRight,
-                    //         transform: Matrix4.skewX(0.1)..rotateX(rotateX),
-                    //         child: Container(
-                    //           padding: const EdgeInsets.all(8.0),
-                    //           color: const Color(0xFFE8581C),
-                    //           child: const Text('测试！'),
-                    //         ),
-                    //       ),
-                    //     ),
-                    //   ),
-                    // ),
-                    const SizedBox(
-                      height: 8.0,
-                    ),
-                    Material(
-                      child: SizedBox(
-                        width: 100,
-                        height: 10,
-                        child: Slider(
-                            value: value,
-                            onChanged: (v) {
-                              setState(() {
-                                rotateX = 2 * pi * v;
-                                value = v;
-                              });
-                              Future.delayed(Duration.zero, () {
-                                final bound =
-                                    key.currentContext?.findRenderObject()
-                                        as RenderRepaintBoundary;
-                                // bound.toImage().then((value) async {
-                                //   final img = await value.toByteData(
-                                //       format: ui.ImageByteFormat.png);
-                                //   setState(() {
-                                //     this.img = img?.buffer.asUint8List();
-                                //   });
-                                // });
-                              });
-                            }),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 8.0,
-                    ),
-
-                    Column(
-                      children: [
-                        TextButton(
-                            onPressed: () {
-                              final bound = key.currentContext
-                                  ?.findRenderObject() as RenderRepaintBoundary;
-                              // bound.toImage().then((value) async {
-                              //   final img = await value.toByteData(
-                              //       format: ui.ImageByteFormat.png);
-                              //   setState(() {
-                              //     this.img = img?.buffer.asUint8List();
-                              //   });
-                              // });
-                            },
-                            child: const Text("Catch Images")),
-                        img == null
-                            ? Text("img")
-                            : Image.memory(
-                                img!,
-                                width: 100,
-                                height: 100,
-                                fit: BoxFit.contain,
-                              )
-                      ],
-                    )
-                  ],
-                ),
-              )
-            ],
-          ))
-        ],
+    var innerBox = SizedBox(
+      width: 100,
+      height: 100,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 100, sigmaY: 100),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+                color: Colors.blue.withAlpha(230)
+            ),
+              ),
+        ),
       ),
+    );
+    var outerBox = SizedBox(
+      width: 200,
+      height: 200,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 100, sigmaY: 100),
+          child: Container(
+            color: Colors.red.withAlpha(10),
+            child: Stack(
+              children: [
+                innerBox,
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+    return Stack(
+      children: [
+        Image.network('https://cataas.com/cat'),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: outerBox,
+        ),
+      ],
     );
   }
 }
